991b59c0a2bd3efa5b465b97945a3669f7b685c5
RAMZI <ramzi446@hotmail.com>
2021-06-26 13:15:18 +0200

    26/06/2021

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 
 const Home = () => {
+    let parallax = (e)=>{
+        document.querySelectorAll('.layer ').forEach(layer =>{
+            const speed = layer.getAttribute("data-speed")
+            const x =(window.innerWidth - e.pageX*speed)/100
+            const y =(window.innerHeight - e.pageY*speed)/100
+            layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+            console.log(x);
+        })
+    }
     return (
-        <div className="anim">
-                <span className="span1" data-speed="5"><i className="fas fa-music"></i></span>
-                <span className="span2" data-speed="5"><i className='fas fa-coffee'></i></span>
-                <span className="span3" data-speed="5"><i className='fas fa-quote-left'></i></span>
-                <span className="span4" data-speed="5"><i className='fas fa-quote-right'></i></span>
-                <span className="span5" data-speed="5"><i className='fas fa-rocket'></i></span>
-                <span className="span6" data-speed="5"><i className='far fa-lightbulb'></i></span>
-                <span className="span7" data-speed="5"><i className=' far fa-comments'></i></span>
-                <span className="span8" data-speed="5"><i className=' fas fa-robot'></i></span>
-                <span className="span9" data-speed="5"><i className=' fas fa-code'></i></span>
-                <span className="span10" data-speed="5"><i className=' fas fa-bug'></i></span>
+        <div onMouseMove={parallax} className="anim">
+                <span className="span1 layer" data-speed="5"><i className="fas fa-music"></i></span>
+                <span className="span2 layer" data-speed="8"><i className='fas fa-coffee'></i></span>
+                <span className="span3 layer" data-speed="4"><i className='fas fa-quote-left'></i></span>
+                <span className="span4 layer" data-speed="-2"><i className='fas fa-quote-right'></i></span>
+                <span className="span5 layer" data-speed="3"><i className='fas fa-rocket'></i></span>
+                <span className="span6 layer" data-speed="3"><i className='far fa-lightbulb'></i></span>
+                <span className="span7 layer" data-speed="12"><i className=' far fa-comments'></i></span>
+                <span className="span8 layer" data-speed="9"><i className=' fas fa-robot'></i></span>
+                <span className="span9 layer" data-speed="2"><i className=' fas fa-code'></i></span>
+                <span className="span10 layer" data-speed="-1"><i className=' fas fa-bug'></i></span>
             <div >
                 <h1 className="title">Welcome to my Portfolio...</h1>
                 <br/>
